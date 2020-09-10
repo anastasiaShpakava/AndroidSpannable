623ec261dbda4f17e9f79eadac9cb7ab5b8a75a5
anastasiaShpakava <shpakavaa@gmail.com>
2020-09-10 10:04:36 +0300

    added Contact.java

---
+++ app/src/main/java/com/company/androidspannable/Contact.java
@@ -0,0 +1,47 @@
+package com.company.androidspannable;
+
+public class Contact {
+    private int image;
+    private String name;
+    private String number;
+
+    public Contact(int image, String name, String number) {
+        this.image = image;
+        this.name = name;
+        this.number = number;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "image=" + image +
+                ", name='" + name + '\'' +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
+
