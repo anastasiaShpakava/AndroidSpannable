9edac3cb4f426bd3e7779e1189144967bbccb0d4
anastasiaShpakava <shpakavaa@gmail.com>
2020-09-10 10:23:49 +0300

    added ContactAdapter.java, SecondActivity.java, list_item.xml, secondary_activity.xml, changed MainActivity to ListActivity.java, activity_main.xml to activity_list.xml

---
+++ app/src/main/java/com/company/androidspannable/ContactAdapter.java
@@ -0,0 +1,70 @@
+package com.company.androidspannable;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder> {
+    private LayoutInflater layoutInflater;
+    private List<Contact> contacts;
+    private Context context;
+
+    public ContactAdapter(Context context, List<Contact> contacts) {
+        this.contacts = contacts;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ContactAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(ContactAdapter.ViewHolder holder, final int position) {
+        final Contact contact = contacts.get(position);
+        holder.imageView.setImageResource(contact.getImage());
+        holder.nameView.setText(contact.getName());
+        holder.numberView.setText(contact.getNumber());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, SecondActivity.class);
+                intent.putExtra("number", contact.getNumber());
+                intent.putExtra("photoId",contact.getImage());
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return contacts.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        final ImageView imageView;
+        final TextView nameView, numberView;
+
+        ViewHolder(View view) {
+            super(view);
+            imageView = view.findViewById(R.id.image);
+            nameView = view.findViewById(R.id.name);
+            numberView = view.findViewById(R.id.number);
+        }
+    }
+}
+
---
+++ app/src/main/java/com/company/androidspannable/SecondActivity.java
@@ -0,0 +1,47 @@
+package com.company.androidspannable;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    private TextView textNumber;
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondary_activity);
+        getSupportActionBar().hide();
+        String number = getIntent().getStringExtra("number");
+        textNumber = findViewById(R.id.textNumber);
+        textNumber.setText(number);
+        imageView = findViewById(R.id.imageV);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            int resId = bundle.getInt("photoId");
+            imageView.setImageResource(resId);
+        }
+    }
+
+    public void dial(View v) {
+        String toDial = "tel:" + textNumber.getText().toString();
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.parse(toDial));
+        startActivity(callIntent);
+    }
+    String subject = "Test";
+    String message = "Test";
+
+    public void sendEmail (View v){
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"shpakavaa@gmail.com"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+        emailIntent.setType("message/rfc822");
+        startActivity(emailIntent);
+    }
+}
